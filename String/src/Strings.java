279ca36e0652e9164e2969e9e9ea3dc279ae91a1
ksadej <66494934+ksadej@users.noreply.github.com>
2021-03-29 15:45:21 +0200

    first commit

---
+++ String/src/Strings.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class Strings {
+
+	//http://www.jkozak.pl/przedmioty/java/zadania4.pdf
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		//zd.1
+//		System.out.println("Type a word:");
+//		
+//		String word = scan.nextLine();
+//		System.out.println("The word:"+word);
+//		String lchar = word.substring(word.length()-1);
+//		System.out.println("The last letter is: "+lchar);
+//		System.out.println("In word are contained: "+word.indexOf(lchar)+1+" letter "+lchar );
+		
+		
+		//zd. 2
+//		System.out.println("Word: ");
+//		String word = scan.nextLine();
+//		String[] strArray = word.split("");
+//				
+//		for (int i=0; i<strArray.length; i++) {
+//			System.out.println(strArray[i]);
+//		}
+//		String rev ="";
+//		System.out.println("-----------------");
+//		for (int i=strArray.length -1; i>-1; i--) {
+//			System.out.println(strArray[i]);
+//		}
+		
+		//zd.3
+//		System.out.println("Word: ");
+//		String word = scan.nextLine();
+//		String[] strArray = word.split("");
+//				
+//		for (int i=0; i<strArray.length; i++) {
+//			System.out.println(strArray[i]);
+//		}
+//		String rev ="";
+//		System.out.println("-----------------");
+//		
+//		for (int i=strArray.length -1; i>-1; i--) 
+//			rev = rev + word.charAt(i);			
+//		
+//		if(word.equals(rev)) {
+//			System.out.println("true");
+//		}else {
+//			System.out.println("false");
+//		}
+		
+		//zd.4
+//		String word = "22wzz";
+//		int sum =0;
+//		for (int i=0; i<word.length(); i++) {
+//			if(Character.isDigit(word.charAt(i))) {
+//				sum+=word.charAt(i)-'0';
+//			}
+//		}
+//		System.out.println("Sum of the numbers in string: "+word+" -> "+sum);
+		
+		
+	}
+}
